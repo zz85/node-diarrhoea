8d10d4e57c041461165039fae7fda2a0cb078577
Joshua Koo <zz85nus@gmail.com>
2015-09-10 08:28:24 +0800

    Make Client class imitate Socket class

--- examples/open.js
+++ examples/open.js
@@ -1,8 +1,6 @@
 var uds = require('../index');
 
-var c = new uds.Client();
-client = c.connect('./test.sock');
+var client = new uds.Client();
 
-// console.log(client);
-
-client.post_message('ping', Math.random())
+client.connect('./test.sock');
+client.post_message('ping', Math.random());
--- examples/start.js
+++ examples/start.js
@@ -1,10 +1,13 @@
+// simple server example
 var uds = require('../index');
 
-var server = new uds.Server({}, function(socket) {
-	console.log('meow');
-	socket.on('rpc', function() {
+var server = new uds.Server();
+
+server.on('connection', function(socket) {
+	console.log('new connection');
+	socket.on('rpc', function(method, params) {
 		console.log('on rpc', arguments);
-	})
+	});
 });
 
 server.listen('./test.sock');
